771827e8e67ce3e563adc3dad706274694e2bc58
Ren Ivey <haleyrivey@gmail.com>
2021-07-09 15:08:37 -0400

    Got file to generate with user input.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# readme-generator
-Node.js Homework: Professional README Generator
+# README Generator
+
+## Description
+An application used for automatically generating styled README file for projects, sothat  manual markdown isn't necessary each time.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Tests](#tests)
+- [Questions?](#questions?)
+- [License](#license)
+
+## Installation
+Inquirer is necessary for proper functionality inside Node.js
+
+## Usage
+--  
+![Application screenshot](screenshot.png)
+
+## Credits
+Starter code provided by Georgia Tech Boot Camps.
+
+## Tests
+--
+
+## Questions?
+[Find me on Github](https://github.com/hr-ivey)  
+[Email me](mailto:haleyrivey@gmail.com)
+
+---
+## License
+A
---
+++ index.js
@@ -0,0 +1,102 @@
+// TODO: Include packages needed for this application
+const inquirer = require ('inquirer');
+const fs = require('fs');
+
+// TODO: Create a function to write README file
+const generateTXT = (answers) =>
+`# ${answers.project_title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Tests](#tests)
+- [Questions?](#questions?)
+- [License](#license)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}  
+![Application screenshot](screenshot.png)
+
+## Credits
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions?
+[Find me on Github](https://github.com/${answers.username})  
+[Email me](mailto:${answers.email})
+
+---
+## License
+${answers.license}`;
+
+
+// TODO: Create an array of questions for user input
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            name: 'project_title',
+            message: "What is the title of your project",
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: "Please enter a project description",
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: "Please enter installation instructions",
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: "Please enter usage information",
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: "Please enter names of other contributors",
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: "Please enter test instructions",
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Select a license',
+            choices: [
+                'A', new inquirer.Separator(),
+                'B', new inquirer.Separator(),
+                'C',
+            ],
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: "Please enter your GitHub username",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter your email address",
+        },
+])
+.then((answers) => {
+    let rmContent = generateTXT(answers);
+
+    fs.writeFile('SAMPLE.MD', rmContent, (err) =>
+    err ? console.log(err) : console.log("Successfully generated README file")
+    );
+});
